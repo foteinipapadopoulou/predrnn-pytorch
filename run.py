800f63aa0f19670e692901251bb24852f601d49b
Leonidas <leonidaskaldanis@hotmail.com>
2024-07-03 15:23:15 +0200

    hyperopt code (currently not running)

--- run.py
+++ run.py
@@ -9,13 +9,14 @@ from core.data_provider import datasets_factory
 from core.models.model_factory import Model
 from core.utils import preprocess
 import core.trainer as trainer
+from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
 
 # -----------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='PyTorch video prediction model - PredRNN')
 
 # training/test
 parser.add_argument('--is_training', type=int, default=1)
-parser.add_argument('--device', type=str, default='cpu:0')
+parser.add_argument('--device', type=str, default='cuda')
 
 # data
 parser.add_argument('--dataset_name', type=str, default='mnist')
@@ -29,7 +30,7 @@ parser.add_argument('--img_width', type=int, default=64)
 parser.add_argument('--img_channel', type=int, default=1)
 
 # model
-parser.add_argument('--model_name', type=str, default='predrnn')
+parser.add_argument('--model_name', type=str, default='predrnn_v2')
 parser.add_argument('--pretrained_model', type=str, default='')
 parser.add_argument('--num_hidden', type=str, default='64,64,64,64')
 parser.add_argument('--filter_size', type=int, default=5)
@@ -53,10 +54,10 @@ parser.add_argument('--sampling_changing_rate', type=float, default=0.00002)
 parser.add_argument('--lr', type=float, default=0.001)
 parser.add_argument('--reverse_input', type=int, default=1)
 parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--max_iterations', type=int, default=80000)
+parser.add_argument('--max_iterations', type=int, default=1000)
 parser.add_argument('--display_interval', type=int, default=100)
-parser.add_argument('--test_interval', type=int, default=5000)
-parser.add_argument('--snapshot_interval', type=int, default=5000)
+parser.add_argument('--test_interval', type=int, default=500)
+parser.add_argument('--snapshot_interval', type=int, default=500)
 parser.add_argument('--num_save_samples', type=int, default=10)
 parser.add_argument('--n_gpu', type=int, default=1)
 
@@ -77,88 +78,22 @@ parser.add_argument('--blur', type=int, default=0)
 parser.add_argument('--affine', type=int, default=0)
 parser.add_argument('--resized_crop', type=int, default=0)
 parser.add_argument('--random_perspective', type=int, default=0)
-parser.add_argument('--loss_filename', type=str, default='loss')
 
 args = parser.parse_args()
-print(args)
 
-
+# -----------------------------------------------------------------------------
 def reserve_schedule_sampling_exp(itr):
+    r_eta = 0.5
     if itr < args.r_sampling_step_1:
-        r_eta = 0.5
+        eta = 1.0
     elif itr < args.r_sampling_step_2:
-        r_eta = 1.0 - 0.5 * math.exp(-float(itr - args.r_sampling_step_1) / args.r_exp_alpha)
+        eta = 1.0 - (1.0 - r_eta) * (itr - args.r_sampling_step_1) / (args.r_sampling_step_2 - args.r_sampling_step_1)
     else:
-        r_eta = 1.0
-
-    if itr < args.r_sampling_step_1:
-        eta = 0.5
-    elif itr < args.r_sampling_step_2:
-        eta = 0.5 - (0.5 / (args.r_sampling_step_2 - args.r_sampling_step_1)) * (itr - args.r_sampling_step_1)
-    else:
-        eta = 0.0
-
-    r_random_flip = np.random.random_sample(
-        (args.batch_size, args.input_length - 1))
-    r_true_token = (r_random_flip < r_eta)
-
-    random_flip = np.random.random_sample(
-        (args.batch_size, args.total_length - args.input_length - 1))
+        eta = r_eta
+    random_flip = np.random.random_sample((args.batch_size, args.total_length - args.input_length - 1))
     true_token = (random_flip < eta)
-
-    ones = np.ones((args.img_width // args.patch_size,
-                    args.img_width // args.patch_size,
-                    args.patch_size ** 2 * args.img_channel))
-    zeros = np.zeros((args.img_width // args.patch_size,
-                      args.img_width // args.patch_size,
-                      args.patch_size ** 2 * args.img_channel))
-
-    real_input_flag = []
-    for i in range(args.batch_size):
-        for j in range(args.total_length - 2):
-            if j < args.input_length - 1:
-                if r_true_token[i, j]:
-                    real_input_flag.append(ones)
-                else:
-                    real_input_flag.append(zeros)
-            else:
-                if true_token[i, j - (args.input_length - 1)]:
-                    real_input_flag.append(ones)
-                else:
-                    real_input_flag.append(zeros)
-
-    real_input_flag = np.array(real_input_flag)
-    real_input_flag = np.reshape(real_input_flag,
-                                 (args.batch_size,
-                                  args.total_length - 2,
-                                  args.img_width // args.patch_size,
-                                  args.img_width // args.patch_size,
-                                  args.patch_size ** 2 * args.img_channel))
-    return real_input_flag
-
-
-def schedule_sampling(eta, itr):
-    zeros = np.zeros((args.batch_size,
-                      args.total_length - args.input_length - 1,
-                      args.img_width // args.patch_size,
-                      args.img_width // args.patch_size,
-                      args.patch_size ** 2 * args.img_channel))
-    if not args.scheduled_sampling:
-        return 0.0, zeros
-
-    if itr < args.sampling_stop_iter:
-        eta -= args.sampling_changing_rate
-    else:
-        eta = 0.0
-    random_flip = np.random.random_sample(
-        (args.batch_size, args.total_length - args.input_length - 1))
-    true_token = (random_flip < eta)
-    ones = np.ones((args.img_width // args.patch_size,
-                    args.img_width // args.patch_size,
-                    args.patch_size ** 2 * args.img_channel))
-    zeros = np.zeros((args.img_width // args.patch_size,
-                      args.img_width // args.patch_size,
-                      args.patch_size ** 2 * args.img_channel))
+    ones = np.ones((args.img_width // args.patch_size, args.img_width // args.patch_size, args.patch_size ** 2 * args.img_channel))
+    zeros = np.zeros((args.img_width // args.patch_size, args.img_width // args.patch_size, args.patch_size ** 2 * args.img_channel))
     real_input_flag = []
     for i in range(args.batch_size):
         for j in range(args.total_length - args.input_length - 1):
@@ -167,20 +102,41 @@ def schedule_sampling(eta, itr):
             else:
                 real_input_flag.append(zeros)
     real_input_flag = np.array(real_input_flag)
-    real_input_flag = np.reshape(real_input_flag,
-                                 (args.batch_size,
-                                  args.total_length - args.input_length - 1,
-                                  args.img_width // args.patch_size,
-                                  args.img_width // args.patch_size,
-                                  args.patch_size ** 2 * args.img_channel))
-    return eta, real_input_flag
+    real_input_flag = np.reshape(real_input_flag, (args.batch_size, args.total_length - args.input_length - 1,
+                                                   args.img_width // args.patch_size, args.img_width // args.patch_size,
+                                                   args.patch_size ** 2 * args.img_channel))
+    return real_input_flag
 
+def schedule_sampling(eta, itr):
+    zeros = np.zeros((args.batch_size, args.total_length - args.input_length - 1,
+                      args.img_width // args.patch_size, args.img_width // args.patch_size,
+                      args.patch_size ** 2 * args.img_channel))
+    if not args.scheduled_sampling:
+        return 1.0, zeros
+    if itr < args.sampling_stop_iter:
+        eta -= args.sampling_changing_rate
+    else:
+        eta = 0.0
+    random_flip = np.random.random_sample((args.batch_size, args.total_length - args.input_length - 1))
+    true_token = (random_flip < eta)
+    ones = np.ones((args.img_width // args.patch_size, args.img_width // args.patch_size, args.patch_size ** 2 * args.img_channel))
+    real_input_flag = []
+    for i in range(args.batch_size):
+        for j in range(args.total_length - args.input_length - 1):
+            if true_token[i, j]:
+                real_input_flag.append(ones)
+            else:
+                real_input_flag.append(zeros)
+    real_input_flag = np.array(real_input_flag)
+    real_input_flag = np.reshape(real_input_flag, (args.batch_size, args.total_length - args.input_length - 1,
+                                                   args.img_width // args.patch_size, args.img_width // args.patch_size,
+                                                   args.patch_size ** 2 * args.img_channel))
+    return eta, real_input_flag
 
 def train_wrapper(model):
     if args.pretrained_model:
         model.load(args.pretrained_model)
 
-    # Load augmentation arguments
     augmentations = {
         'rotate': args.rotate,
         'random_flip': args.random_flip,
@@ -192,7 +148,6 @@ def train_wrapper(model):
     if all(value == 0 for value in augmentations.values()):
         augmentations = None
 
-    # load data
     train_input_handle, test_input_handle = datasets_factory.data_provider(
         args.dataset_name, args.train_data_paths, args.valid_data_paths, args.batch_size, args.img_width,
         seq_length=args.total_length, injection_action=args.injection_action, is_training=True, augmentations=augmentations)
@@ -220,7 +175,6 @@ def train_wrapper(model):
 
         train_input_handle.next()
 
-
 def test_wrapper(model):
     model.load(args.pretrained_model)
     test_input_handle = datasets_factory.data_provider(
@@ -228,20 +182,65 @@ def test_wrapper(model):
         seq_length=args.total_length, injection_action=args.injection_action, is_training=False)
     trainer.test(model, test_input_handle, args, 'test_result')
 
+# Define the hyperparameter space
+space = {
+    'lr': hp.loguniform('lr', -10, -2),
+    'num_hidden': hp.choice('num_hidden', ['64,64,64,64', '128,128,128,128']),
+    'filter_size': hp.choice('filter_size', [3, 5]),
+    'stride': hp.choice('stride', [1, 2])
+}
 
-if os.path.exists(args.save_dir):
-    shutil.rmtree(args.save_dir)
-os.makedirs(args.save_dir)
+def objective(params):
+    # Update args with the current hyperparameters
+    args.lr = params['lr']
+    args.num_hidden = params['num_hidden']
+    args.filter_size = params['filter_size']
+    args.stride = params['stride']
 
-if os.path.exists(args.gen_frm_dir):
-    shutil.rmtree(args.gen_frm_dir)
-os.makedirs(args.gen_frm_dir)
+    print(f"Current Hyperparameters: {params}")
 
-print('Initializing models')
+    # Initialize model
+    model = Model(args)
 
-model = Model(args)
+    # Load training data
+    train_input_handle, _ = datasets_factory.data_provider(
+        args.dataset_name, args.train_data_paths, args.valid_data_paths, args.batch_size, args.img_width,
+        seq_length=args.total_length, injection_action=args.injection_action, is_training=True)
 
-if args.is_training:
-    train_wrapper(model)
-else:
-    test_wrapper(model)
+    eta = args.sampling_start_value
+
+    total_cost = 0
+    for itr in range(1, args.max_iterations + 1):
+        if train_input_handle.no_batch_left():
+            train_input_handle.begin(do_shuffle=True)
+        ims = train_input_handle.get_batch()
+        ims = preprocess.reshape_patch(ims, args.patch_size)
+
+        if args.reverse_scheduled_sampling == 1:
+            real_input_flag = reserve_schedule_sampling_exp(itr)
+        else:
+            eta, real_input_flag = schedule_sampling(eta, itr)
+
+        cost = trainer.train(model, ims, real_input_flag, args, itr)
+        total_cost += cost
+
+        train_input_handle.next()
+
+    avg_cost = total_cost / args.max_iterations
+    return {'loss': avg_cost, 'status': STATUS_OK}
+
+
+if __name__ == '__main__':
+    # Initialize trials object to store results
+    trials = Trials()
+
+    # Run the optimization
+    best = fmin(
+        fn=objective,
+        space=space,
+        algo=tpe.suggest,
+        max_evals=50,  # Number of evaluations
+        trials=trials
+    )
+
+    print(f"Best hyperparameters: {best}")
